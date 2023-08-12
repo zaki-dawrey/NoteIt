53d48617a18ab56b4e1d8306e1dce3f9bdafea2a
Zaki Dawrey <zdawrey@gmail.com>
2023-08-12 22:27:11 +0530

    Displaying Notes in Notes View

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -12,11 +12,17 @@ class NotesService {
   List<DatabaseNote> _notes = [];
 
   static final NotesService _shared = NotesService._sharedInstace();
-  NotesService._sharedInstace();
+  NotesService._sharedInstace() {
+    _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
+      onListen: () {
+        _notesStreamController.sink.add(_notes);
+      },
+    );
+  }
+
   factory NotesService() => _shared;
 
-  final _notesStreamController =
-      StreamController<List<DatabaseNote>>.broadcast();
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
 
