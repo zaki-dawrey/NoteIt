ed7daada346a330639e3b267b820d31384c28030
Zaki Dawrey <zdawrey@gmail.com>
2023-09-09 23:27:13 +0530

    Bloc Added

---
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_application_1/services/auth/auth_provider.dart';
+import 'package:flutter_application_1/services/auth/bloc/auth_event.dart';
+import 'package:flutter_application_1/services/auth/bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+    on<AuthEventInitialize>((event, emit) async {
+      await provider.initialize();
+      final user = provider.currentUser;
+      if (user == null) {
+        emit(const AuthStateLoggedOut());
+      } else if (!user.isEmailVerified) {
+        emit(const AuthStateNeedsVerification());
+      } else {
+        emit(AuthStateLoggedIn(user));
+      }
+    });
+  }
+}
